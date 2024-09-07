573cb51d9f628e2032d27782e7c103ef96795428
ayansheikh6600 <ayansheikh6600@gmail.com>
2024-09-08 02:30:23 +0500

    "Some Chnages"

--- script.ts
+++ script.ts
@@ -4,9 +4,9 @@ const skillsSection = document.getElementById("skills") as HTMLElement;
 toggleSkillsButton.addEventListener("click", () => {
   if (skillsSection.style.display === "none") {
     skillsSection.style.display = "block";
-    toggleSkillsButton.innerText = "Open Skill Section"
+    toggleSkillsButton.innerText = "Close Skill Section"
   } else {
     skillsSection.style.display = "none";
-    toggleSkillsButton.innerText = "Close Skill Section"
+    toggleSkillsButton.innerText = "Open Skill Section"
   }
 });
